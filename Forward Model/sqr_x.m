62c1a94623155cbac33b98332b38c1de14bae443
ahmd-mahm <ahmedmahmood84@gmail.com>
2018-12-10 03:31:54 +0800

    Stable squared updated with appropriate PDFs

--- Forward Model/sqr_x.m
+++ Forward Model/sqr_x.m
@@ -11,7 +11,7 @@ xdB_min = ydB_min/2;    % 10log10()
 xdB_max = ydB_max/2;
 
 ydB = ydB_min:(ydB_max-ydB_min)/nbins:ydB_max;
-x = x_min:(x_max-x_min)/nbins:x_max;
+%x = x_min:(x_max-x_min)/nbins:x_max;
 xdB = xdB_min:(xdB_max-xdB_min)/nbins:xdB_max;
 
 pd=makedist('Stable','alpha',1.5,'beta',0,'gam',1,'delta',0);
@@ -21,11 +21,12 @@ x_rnd_dB=pow2db(x_rnd); % 10log10()
 y_rnd_dB=mag2db(x_rnd); % 20log10() => y=x^2
 
 figure
+subplot(2,1,1)
 y_rnd=x_rnd.^2;
 %y=x_min^2:(x_max^2-x_min^2)/nbins:x_max^2;
-y_int=quantile(y_rnd,[10^-4, 1-10^-4 ]);
-y= y_int(1):(y_int(2)-y_int(1))/nbins:y_int(2);
-%y=0:0.01/nbins:0.01;
+%y_int=quantile(y_rnd,[10^-4, 1-10^-4 ]);
+%y= log10(y_int(1):(y_int(2)-y_int(1))/nbins:y_int(2);
+y= 10.^(ydB/10);
 histogram(y_rnd,y,'normalization','pdf');
 grid on
 hold on
@@ -33,13 +34,14 @@ y_bins=(y(1:end-1)+y(2:end))/2;
 pdf_y= pdf(pd,sqrt(y_bins))./sqrt(y_bins); % y=x^2;
 plot(y_bins,pdf_y,'linewidth',2)
 set(gca,'yscale','log')
+set(gca,'xscale','log')
 xlabel('x^2')
 
 % figure
 % histogram(y_rnd_dB,ydB,'normalization','pdf');
 % grid on
 % 
-figure
+subplot(2,1,2)
 histogram(y_rnd_dB,ydB,'normalization','pdf');
 set(gca,'yscale','log')
 grid on
@@ -56,12 +58,12 @@ G(1:2)
 
 figure
 subplot(2,1,1)
-%histogram(x_rnd,x,'normalization','pdf');
-
+%x=-5:9/nbins:4;
+x=xdB/10;
+x=10.^(x);
+histogram(x_rnd,x,'normalization','pdf');
 grid on
 hold on
-x=-20:120/200:100;
-x=10.^(x);
 pdf_x= 2*pdf(pd,x);
 plot(x,pdf_x,'linewidth',2)
 set(gca,'yscale','log')
