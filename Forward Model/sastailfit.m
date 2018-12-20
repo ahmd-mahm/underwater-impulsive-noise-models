0ca0f0c960065b38b890c4867a141a1a4463201a
ahmd-mahm <ahmedmahmood84@gmail.com>
2018-12-21 01:41:39 +0800

    added test function for sastailfit.m

--- Forward Model/sastailfit.m
+++ Forward Model/sastailfit.m
@@ -1,24 +1,36 @@
 function [alpha,delta]=sastailfit(x,L)
+% fits the tail of the empirical pdf computed from 'x', where L(1) and L(2)
+% highlight the percentile data to be considered in the tail. Note that
+% L(1)<L(2) and both lie within [0,1]
 
-lg_x=log(abs(x));
+x=abs(x);
 nbins=50;
-if ~isempty(L)
-    L=log(L);
-    edges=L(1):(L(2)-L(1))/nbins:L(2);
+if nargin==2
+    qnt=log(quantile(x,L));
 else
-    qnt=(quantile(lg_x,[0.8, 0.95]));
-    edges=qnt(1):(qnt(2)-qnt(1))/nbins:qnt(2);
+    qnt=log(quantile(x,[0.99, 0.999]));
 end
 
+edges=qnt(1):(qnt(2)-qnt(1))/nbins:qnt(2);
 bins=(edges(1:end-1)+edges(2:end))/2;
-lg_epdf=histcounts(lg_x,edges,'normalization','pdf');
+edges=exp(edges);
+
+lg_epdf=log(histcounts(abs(x),edges,'normalization','pdf')).';
+
+% figure
+% plot(bins,lg_epdf)
 
 X=[bins.', ones(nbins,1)];
-
 coeff=(X.'*X)\(X.'*lg_epdf);
 
 grad= coeff(1);
 c= coeff(2);
+
+%hold on
+%plot(bins,grad*bins+c)
+hold on
+plot(exp(bins),exp(grad*bins+c),'linewidth',2)
+
 
 alpha=-grad-1;
 Ca=gamma(alpha)*sin(alpha*pi/2)/pi;
