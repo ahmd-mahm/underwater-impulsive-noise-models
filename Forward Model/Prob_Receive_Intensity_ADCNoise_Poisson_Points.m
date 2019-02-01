a8dee50535d4cea46036341178768b110106a0da
Ahmed M <ahmedmahmood84@gmail.com>
2019-02-01 19:46:06 +0800

    Added silhouette of a snap and updated ADC noise point picking script

--- Forward Model/Prob_Receive_Intensity_ADCNoise_Poisson_Points.m
+++ Forward Model/Prob_Receive_Intensity_ADCNoise_Poisson_Points.m
@@ -1,5 +1,4 @@
 clc; clear; close all
-
 % 90 dB noise / ADC res noise
 % set a lambda value
 % Get an impulse train for transmission times, resolve at 180 kHz 
@@ -14,13 +13,25 @@ clc; clear; close all
 % of transmitted intensity. The script outputs the distribution of
 % transmitted intensity.
 
-d=5;        % sensor depth
-h=20;       % height of water column
-N=10^7;     % number of points
-alpha=10;   % absorption coefficient in dB/km
-x_max=200;
+%% *** Initial Settings ***
+d=5;        % sensor depth in m
+h=20;       % height of water column in m
+c=1500;     % speed of sund in water in m/s
+samples=5*10^6;   % considered time samples
+fs=180000;  % samping frequency in Hz
 
-lambda=20; % snaps per second
+alpha=10;   % absorption coefficient in dB/km
+lambda=20;  % snaps per second
+T=samples/fs; % associated time window in s
+
+%N=round(1.1*lambda*T); % number of points (snaps)
+
+disp(['d = ',num2str(d),' m']);
+disp(['h = ',num2str(h),' m']);
+disp(['T = ',num2str(T),' secs']);
+
+
+%% *** ADC Noise ***
 
 % x=loadHifDAQ3('F:\HifDAQ 2014\HIDAQ_2014\Aug_15_2014_2',0,20,1);
 % x=x(:,1);
@@ -32,103 +43,93 @@ lambda=20; % snaps per second
 % g=z(z~=0);
 % ADC_noise=20*log10(min(g));
 
-ADC_noise_I= 84.7; % in dB
+ADC_noise_I= 84.7; % in dB % ADC Noise
 
-alp=1.53;
-del=2.282e+05;
-pd=makedist('Stable','alpha',alp,'beta',0,'gam',del,'delta',0);
-disp(['alpha = ',num2str(alp)]);
-disp(['delta = ',num2str(del)]);
+%% *** Evaluating x_max, tau_max, tau_min, and N ***
 
-Pr=random(pd,1,N);
-Pr_abs=abs(Pr);
-Ir_dB=20*log10(Pr_abs);
+It_mean_dB=180; % in dB
+It_var_dB=(10/3)^2;
 
-x_max=
+TL_dB_max=It_mean_dB+3*sqrt(It_var_dB);
+if alpha~=0
+    %r=(20000*wrightOmega(it_dB/20 - Ir_dB/20 - log(20000/alpha)))/alpha;
+    r_max=(20000*wrightOmega(TL_dB_max/20 - log(20000/alpha)))/alpha;
+else
+    r_max=10.^(TL_dB_max)/20;
+end
+x_max=sqrt(r_max^2-(h-d)^2);
+tau_max=r_max/c;
+tau_min=(h-d)/c;
 
+Tx_interval=T-tau_min+tau_max; % transmission time window
+N=round(lambda*Tx_interval); 
+
+disp(['N = ',num2str(N),' snaps']);
+
+%% *** PDF/CDF Transmission Loss (dB) ***
+% TL_dB=it_dB-Ir_dB;
+% 
+% L=[10^-5,1-10^-5];
+% nbins=180;
+% figure
+% pdfquant(TL_dB,nbins,L);
+% xlabel('TL (dB) -- direct arrivals')
+% 
+% figure
+% [f,bins]=cdfquant(TL_dB,nbins,L);
+% xlabel('TL (dB) -- direct arrivals')
+% 
+% figure
+% plot(f.Values,bins,'.','MarkerSize',16)
+% hold on
+% pp=pchip(f.Values,bins);
+% ind_interp=f.Values(1):0.001:f.Values(end);
+% pp_interp=ppval(pp,ind_interp);
+% plot(ind_interp,pp_interp,'LineWidth',2)
+% grid on
+% ylabel('TL (dB) -- direct arrivals')
+% xlabel('CDF')
+% hold off
+% 
+% 
+% TL=10.^(TL_dB/10);
+% figure
+% logypdfquant(TL,nbins,[10^-4,1-10^-4]);
+% xlabel('TL -- direct arrivals')
+
+%% *** Point Picking ***
+
+x=sqrt(rand(1,N))*x_max;
+phi=2*pi*rand(1,N);
+x_cmp=x.*exp(1i*phi);
+r=sqrt(x.^2+(h-d)^2);
+
+t_ind_Tx=rand(1,N)*(Tx_interval)-tau_max;
+t_ind_Rx= r/c+t_ind_Tx;
+t_ind_Rx= t_ind_Rx(and(t_ind_Rx>=0,t_ind_Rx<=T));
+
+ind_Rx=round(t_ind_Rx*fs);
+N=length(ind_Rx);
+r=r(1:N);
+
+%% *** Generating Transmit Intensities ***
+It_dB=It_mean_dB+(randn(1,N))*sqrt(It_var_dB); % log-normal distribution of intensity
+%it_dB=190;
 
 % in dB : Ir_dB = It_dB - 20*log10(r) - alpha*(r/1000). => alpha is in dB/km
 % linear: Ir = It * (r^-2) * 10^(- alpha*r/(1000*10))
 
-It_mean_dB=250; % in dB
-It_var_dB=(10/3)^2;
-it_dB=It_mean_dB+(randn(1,N))*sqrt(It_var_dB); % log-normal distribution of intensity
-%it_dB=190;
-
-TL_dB=it_dB-Ir_dB;
-
-L=[10^-5,1-10^-5];
-nbins=180;
-figure
-pdfquant(TL_dB,nbins,L);
-xlabel('TL (dB) -- direct arrivals')
+Ir_dB = It_dB - 20*log10(r) - alpha*(r/1000);
+Ir = 10.^(Ir_dB/10);
+Ir_ts=zeros(1,samples);
+Ir_ts(ind_Rx)=Ir;
 
 figure
-[f,bins]=cdfquant(TL_dB,nbins,L);
-xlabel('TL (dB) -- direct arrivals')
-
-figure
-plot(f.Values,bins,'.','MarkerSize',16)
-hold on
-pp=pchip(f.Values,bins);
-ind_interp=f.Values(1):0.001:f.Values(end);
-pp_interp=ppval(pp,ind_interp);
-plot(ind_interp,pp_interp,'LineWidth',2)
+stem((0:1/fs:T-1/fs),Ir_ts)
 grid on
-ylabel('TL (dB) -- direct arrivals')
-xlabel('CDF')
-hold off
 
-
-TL=10.^(TL_dB/10);
+load('silhouette.mat');
+Ir_ts=conv(Ir_ts,silh);
 figure
-logypdfquant(TL,nbins,[10^-4,1-10^-4]);
-xlabel('TL -- direct arrivals')
-
-points=10^4;
-U=rand(1,2*points);
-U=U(and(U<=f.Values(end),U>=f.Values(1)));
-U=U(1:points);
-TL_dB_rand=ppval(pp,U);
-
-%syms Ir_dB It_dB r alpha
-%eqn = Ir_dB == It_dB - 20*log(r) - alpha*(r/1000);
-%r= solve(eqn,r)
-
-if alpha~=0
-    %r=(20000*wrightOmega(it_dB/20 - Ir_dB/20 - log(20000/alpha)))/alpha;
-    r=(20000*wrightOmega(TL_dB_rand/20 - log(20000/alpha)))/alpha;
-else
-    r=10.^(TL_dB_rand)/20;
-end
-r=r(r>h-d);
-points=length(r);
-
-x=sqrt(r.^2-(h-d)^2);
-phi=2*pi*rand(1,points);
-x_cmp=x.*exp(1i*phi);
-% 
-ppickingcircle(x_cmp(1:min(10^4,points)),x_max)
-title(['Total Points = ', num2str(points)])
-
-figure
-[~,bins]=loglogpdfquant(Pr_abs,nbins,L);
-fig_hist=gcf;
-hold on
-pdf_act=pdf(pd,bins);
-plot(bins,2*pdf_act,'linewidth',2)
-xlabel('abs(P_r) -- direct arrivals')
-
-
-function ppickingcircle(x_cmp,rho)
-
-figure
-theta=0:2*pi/500:2*pi;
-[x,y]=pol2cart(theta,repmat(rho,1,length(theta)));
-plot(x,y,'-k','linewidth',2)
-hold on
-
-plot(real(x_cmp),imag(x_cmp),'.','markersize',4)
+stem(Ir_ts)
 grid on
-axis equal
-end
