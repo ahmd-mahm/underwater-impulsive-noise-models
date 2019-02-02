fdd7d997efc801cc2194fea9044353523663aa62
ahmd-mahm <ahmedmahmood84@gmail.com>
2019-02-03 01:13:13 +0800

    Updated ADC noise + point-picking script with surface reflections EUREKA!

--- Forward Model/Prob_Receive_Intensity_ADCNoise_Poisson_Points.m
+++ Forward Model/Prob_Receive_Intensity_ADCNoise_Poisson_Points.m
@@ -12,11 +12,15 @@ clc; clear; close all
 % transmitted intensity.
 
 %% *** Initial Settings ***
+
 d=5;            % sensor depth in m
 h=20;           % height of water column in m
 c=1500;         % speed of sund in water in m/s
 samples=10^6;   % considered time samples
 fs=180000;      % samping frequency in Hz
+
+SR=true;       % with or without surface reflections
+%SR=false;
 
 alpha=10;       % absorption coefficient in dB/km
 rho=0.05;       % between 0.01 and 0.1 snaps/sec/m
@@ -46,7 +50,7 @@ disp(['rho = ',num2str(rho),' snaps/sec/m']);
 %ADC_noise_I= 84.7; % in dB % ADC Noise
 %var_noise=10.^(ADC_noise_I/10);
 
-%% *** Evaluating x_max, tau_max, tau_min, and N ***
+%% *** Evaluating x_max, tau_max, tau_min, lambda and N ***
 
 It_mean_dB=180; % in dB
 It_var_dB=(10/3)^2;
@@ -59,13 +63,32 @@ else
     r_max=10.^(TL_dB_max)/20;
 end
 x_max=sqrt(r_max^2-(h-d)^2);
-tau_max=r_max/c;
-tau_min=(h-d)/c;
 
-Tx_interval=T-tau_min+tau_max; % transmission time window
+
+tau_min=(h-d)/c;
+if SR
+    r_max_sr=sqrt(x_max^2+(h+d)^2);
+    tau_max=r_max_sr/c;
+else
+    tau_max=r_max/c;
+end
+
+Tx_interval=(T-tau_min)+tau_max; % transmission time window: [-tau_max,T-tau_min]
+
+%*****************************
+%******** LAMBDA VALUE********
+%*****************************
+
 lambda=rho*pi*(x_max^2);
+%lambda=10000;
+
+%*****************************
+%*****************************
+
 N=round(lambda*Tx_interval); % number of points (snaps)
 
+disp(['tau_min = ',num2str(tau_min),' secs']);
+disp(['tau_max = ',num2str(tau_max),' secs']);
 disp(['lambda = ',num2str(lambda),' snaps/sec']);
 disp(['N = ',num2str(N),' snaps in [-tau_max,T-tau_min]']);
 disp(['x_max = ',num2str(x_max),' meters']);
@@ -77,62 +100,158 @@ x=sqrt(rand(1,N))*x_max;
 phi=2*pi*rand(1,N);
 x_cmp=x.*exp(1i*phi);
 r=sqrt(x.^2+(h-d)^2);
+if SR
+    r_sr=sqrt(x.^2+(h+d)^2);
+end
 
 %% *** Evaluating Received Snaps' Time Indices ***
 
 t_ind_Tx=rand(1,N)*(Tx_interval)-tau_max;
-t_ind_Rx= r/c+t_ind_Tx;
-t_ind_Rx= t_ind_Rx(and(t_ind_Rx>=0,t_ind_Rx<=T));
+if SR
+    t_ind_Rx= r/c+t_ind_Tx; 
+    t_ind_Rx_sr= r_sr/c+t_ind_Tx;
+    
+    t_ind_logic= and(t_ind_Rx>=0,t_ind_Rx<T-1/(2*fs));      % We want a maximum length of 'samples', hence the -1/2*fs
+    t_ind_sr_logic= and(t_ind_Rx_sr>=0,t_ind_Rx_sr<T-1/(2*fs));% We want a maximum length of 'samples', hence the -1/2*fs
+    
+%     c1=t_ind_Rx(xor(t_ind_logic,t_ind_sr_logic)); c2=t_ind_Rx_sr(xor(t_ind_logic,t_ind_sr_logic));
+%     figure
+%     plot(c1); hold on; plot(c2)
+    
+    t_ind_Rx= t_ind_Rx(t_ind_logic);
+    t_ind_Rx_sr= t_ind_Rx_sr(t_ind_sr_logic);
+    
+    ind_Rx=round(t_ind_Rx*fs);        % resolving time indices to nearest 1/fs;
+    ind_Rx_sr=round(t_ind_Rx_sr*fs);  % resolving time indices to nearest 1/fs;
+    
+    N=length(ind_Rx);                   % actual number of DA snaps
+    N_sr=length(ind_Rx_sr);                   % actual number of DA snaps
+    r=r(t_ind_logic);                   % picking DA poisson points
+    r_sr=r_sr(t_ind_sr_logic);          % picking SR poisson points
+else
+    t_ind_Rx= r/c+t_ind_Tx;
+    
+    t_ind_logic= and(t_ind_Rx>=0,t_ind_Rx<T-1/(2*fs)); % We want a maximum length of 'samples', hence the -1/2*fs
+    
+    t_ind_Rx= t_ind_Rx(t_ind_logic);
+    
+    ind_Rx=round(t_ind_Rx*fs);        % resolving time indices to nearest 1/fs;
+    
+    N=length(ind_Rx);                   % actual number of snaps
+    r=r(t_ind_logic);                   % picking 'N' poisson points
+end
 
-ind_Rx=round(t_ind_Rx*fs);  % resolving time indices to nearest 1/fs;
-N=length(ind_Rx);           % actual number of snaps
-r=r(1:N);                   % picking first N poisson points
 
 %% *** Generating Transmit, ADC Noise and Receive Intensities ***
-
-It_dB=It_mean_dB+(randn(1,N))*sqrt(It_var_dB); % log-normal distribution of intensity
-%it_dB=190;
-Ir_dB_max=It_mean_dB+3*sqrt(It_var_dB) - 20*log10(h-d) - alpha*((h-d)/1000);
-ADC_noise_lvl= (10^(Ir_dB_max/20))/(2^16); % ADC Noise with 16 bits
 
 % in dB : Ir_dB = It_dB - 20*log10(r) - alpha*(r/1000). => alpha is in dB/km
 % linear: Ir = It * (r^-2) * 10^(- alpha*r/(1000*10))
 
-Ir_dB = It_dB - 20*log10(r) - alpha*(r/1000);
-Pr = 10.^(Ir_dB/20);
+It_dB= It_mean_dB+(randn(1,N))*sqrt(It_var_dB); % log-normal distribution of intensity
+%it_dB=190;
+Ir_dB_max= It_mean_dB+3*sqrt(It_var_dB) - 20*log10(h-d) - alpha*((h-d)/1000);
+ADC_noise_lvl= (10^(Ir_dB_max/20))/(2^16); % ADC Noise with 16 bits
 
-Pr_ts=zeros(1,samples);             
-Pr_ts(1+ind_Rx)=Pr;
-load('silhouette.mat');             % Silhouette of an average snap (DA)
-Pr_ts=conv(Pr_ts,silh/max(silh));   % Time-series of received pressure samples
-Ir_ts_dB=20*log10(abs(Pr_ts));
-N_ts=length(Pr_ts);                 % length of Pr_ts
-Pr_ts_ADC=Pr_ts+(rand(1,length(Pr_ts))*2-1)*ADC_noise_lvl/2;    % adding of ADC noise
+Ir_dB= It_dB - 20*log10(r) - alpha*(r/1000);
+Pr= 10.^(Ir_dB/20);
+
+Pr_ts= zeros(1,samples);
+Pr_ts(ind_Rx+1)= Pr;
+load('silhouette.mat');              % Silhouette of an average snap (DA)
+Pr_ts= conv(Pr_ts,silh/max(silh));   % Time-series of received pressure samples
+
+if SR
+    It_dB_sr= It_mean_dB+(randn(1,N_sr))*sqrt(It_var_dB); % log-normal distribution of intensity
+    
+    Ir_dB_sr= It_dB_sr - 20*log10(r_sr) - alpha*(r_sr/1000);
+    Pr_sr= 10.^(Ir_dB_sr/20);
+    
+    Pr_ts_sr= zeros(1,samples);
+    Pr_ts_sr(ind_Rx_sr+1)= -Pr_sr;                % negative pressure
+    Pr_ts_sr= conv(Pr_ts_sr,silh/max(silh));    % time-series of received pressure samples
+    Ir_ts_dB_sr= 20*log10(abs(Pr_ts_sr));
+    
+    Pr_ts_fin= Pr_ts+Pr_ts_sr;
+else
+    Pr_ts_fin= Pr_ts;
+end
+Ir_ts_dB_fin= 20*log10(abs(Pr_ts));
+K_ts= samples+length(silh)-1;                 % length(Pr_ts)= length(Pr_ts_sr)= samples+length(silh)-1;
+
+Pr_ts_ADC=Pr_ts_fin+(rand(1,K_ts)*2-1)*ADC_noise_lvl/2;    % adding of ADC noise
+
 
 %% *** Plots ***
 
 figure
-plot((0:N_ts-1)/fs,Pr_ts)
+plot((0:K_ts-1)/fs,Pr_ts_fin)
 grid on
 xlabel('time')
-ylabel('Pr (waveform)')
+if SR
+    ylabel('Pr (DA+SR) (waveform)')
+else
+    ylabel('Pr (waveform)')
+end
 
 figure
-plot((0:N_ts-1)/fs,Ir_ts_dB)
+plot((0:K_ts-1)/fs,Ir_ts_dB_fin)
 grid on
 xlabel('time')
-ylabel('Ir (dB), waveform')
+if SR
+    ylabel('Ir (dB) (DA+SR), waveform')
+else
+    ylabel('Ir (dB), waveform')
+end
+
 
 figure
 plot((0:length(Pr_ts)-1)/fs,Pr_ts_ADC)
 grid on
 xlabel('time')
-ylabel('P_r + ADC noise (waveform)')
+if SR
+    ylabel('P_r + ADC noise (DA+SR) (waveform)')
+else
+    ylabel('P_r + ADC noise (waveform)')
+end
+
+
 
 %% *** Histograms and PDFs ***
 
 L=[10^-5,1-10^-5];
 nbins=100;
 figure
-loglogpdfquant(abs(Pr_ts(Pr_ts~=0)),nbins,L);
-xlabel('Pr -- direct arrivals')
+loglogpdfquant(abs(Pr_ts_ADC),nbins,L);
+if SR
+    xlabel('Pr + ADC noise (DA+SR)')
+else
+    xlabel('Pr + ADC noise (DA)')
+end
+
+%% *** Point-Picking Function ***
+
+if SR    
+    %ppickingcircle(x_cmp(and(t_ind_logic,t_ind_sr_logic)),x_max,h)
+    ppickingcircle(x_cmp(xor(t_ind_logic,t_ind_sr_logic)),x_max) % Plots those points, that either have a DA or a SR but not both in the received time window [0,T)
+else
+    %ppickingcircle(x_cmp(t_ind_logic),x_max,h)
+end
+
+
+
+%% *** Point-Picking Function ***
+function ppickingcircle(x_cmp,rho,h)
+if nargin==2
+    figure
+else
+    figure(h)
+end
+theta=0:2*pi/500:2*pi;
+[x,y]=pol2cart(theta,repmat(rho,1,length(theta)));
+plot(x,y,'-k','linewidth',2)
+hold on
+
+plot(real(x_cmp),imag(x_cmp),'.','markersize',4)
+grid on
+axis equal
+end
